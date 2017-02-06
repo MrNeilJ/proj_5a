02e4739e56617983a7dddb5db5d20ca9f4cb3535
neiljohnson <mr.neil.j@gmail.com>
2017-02-05 21:03:50 -0800

    Added rest of neccessary files for the assignment (currently blank). Added information to the HPP file

--- Box.hpp
+++ Box.hpp
@@ -5,8 +5,37 @@
 #ifndef PROJ_5A_BOX_HPP
 #define PROJ_5A_BOX_HPP
 
+// Write a class called Box that has three double fields called height, width and length.
+// The class should have set methods for each field.  It should have a three-parameter
+// constructor that takes three doubles and passes them to the set methods to initialize
+// the fields of the Box.  It should have a default constructor that uses the set methods
+// to initialize each field to 1.  It should have a method that calculates and returns the
+// volume of the Box and a method that calculates and returns the surface area of the Box.
+
 
 class Box {
+
+private:
+    int height;
+    int width;
+    int volume;
+
+public:
+    Box()
+    {
+        height = 1;
+        width = 1;
+        volume = 1;
+    }
+    setHeight(int);
+
+    setWidth(int);
+
+    setLength(int);
+
+    getVolume(int);
+
+    getSurfaceArea(int);
 
 };
 
