da2e05f8cf97cc30352a65134d4ab944d0c933c1
Neil Johnson <mr.neil.j@gmail.com>
2017-02-06 14:17:13 -0800

    Added rest of neccessary files for the assignment (currently blank). Added information to the HPP file Adjusted for boolean values Comments added to ensure readability of Box.hpp Typed up and readded information for the boolean values Added the main constructor value for when the user adds in values at creation of the object Added in extra comments to the application for better readability Added comments in for every function as per code guidelines Further adjusted the application information.  (last thing needed was adjustments to the boolean values).
    Adding personal test to see how well the class works

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main() {
     // These are the tests that were provided by the example for the HW
     Box box1(2.4, 7.1, 5.0);
     Box box2;
+    // Added a box for self testing
+    Box boxTest;
     double volume1 = box1.getVolume();
     double surfaceArea1 = box1.getSurfaceArea();
     double volume2 = box2.getVolume();
